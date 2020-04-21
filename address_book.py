d20a576246fc73669eb495081775857d738a27cf
0l9h <olehbobeliak@gmail.com>
2020-04-21 03:39:48 +0300

    adding new person to address_book possibility created

--- address_book.py
+++ address_book.py
@@ -1,3 +1,4 @@
+
 address_book = {
     'Oleg' : '0983528294',
     'Marshal' : '0673489292',
@@ -9,4 +10,14 @@ user_key = input('Input name to get phone number:\n')
 if user_key in address_book:
     print(address_book[user_key])   #   Prints phone number of [user_key] if it exists
 else:
-    print('name', '"'+user_key+'"','isn\'t registered')     #   Ex. of output: name Oleh isn't registered 
+    print('\nname', '"'+user_key+'"','isn\'t registered\n')     #   Ex. of output: name Oleh isn't registered 
+    choice_of_adding_name = input('Input "new" if you want to add new name to register\n\
+If you don\'t want to add new name, print "exit":\n')
+    
+    if choice_of_adding_name == 'new':
+        new_name = input('Input new name:\n')
+        new_phone_number = input('Input new number:\n')
+        address_book[new_name] = new_phone_number     #   Adding new person to address_book
+
+    elif choice_of_adding_name != 'exit':
+        raise NameError('Wrong command')
