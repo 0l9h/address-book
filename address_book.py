c0c6c1b8606c92a086136b2a98f290fb3d430f17
0l9h <olehbobeliak@gmail.com>
2020-04-22 03:22:46 +0300

    Created .json file. New data will remain there

--- address_book.py
+++ address_book.py
@@ -1,23 +1,36 @@
+import json
 
-address_book = {
-    'Oleg' : '0983528294',
-    'Marshal' : '0673489292',
-    'Vasya' : ' 0634901253',
-}
+address_book_file = 'data.json'     #   file containing your data
+
+with open(address_book_file, 'r') as readfile:
+    address_book = json.load(readfile)                   #   Opening of data.json
+
+status = False
 
 user_key = input('Input name to get phone number:\n')
 
-if user_key in address_book:
-    print(address_book[user_key])   #   Prints phone number of [user_key] if it exists
-else:
-    print('\nname', '"'+user_key+'"','isn\'t registered\n')     #   Ex. of output: name Oleh isn't registered 
+for name in address_book[0]['address']:     #   Loop through address_book
+  #  print(name)
+    if user_key in name:
+        print(name[user_key])   #   Prints phone number of [user_key] if it exists
+        status = True
+
+
+if status == False:     #   if phone [user_key] doesn't exists
+    print('\nname', '"'+user_key+'"','isn\'t registered\n')     #   Ex. of output:   name Oleh isn't registered 
     choice_of_adding_name = input('Input "new" if you want to add new name to register\n\
 If you don\'t want to add new name, print "exit":\n')
     
     if choice_of_adding_name == 'new':
         new_name = input('Input new name:\n')
-        new_phone_number = input('Input new number:\n')
-        address_book[new_name] = new_phone_number     #   Adding new person to address_book
+        new_phone_number = input('Input new number:\n')     #   Creating of new data
 
-    elif choice_of_adding_name != 'exit':
-        raise NameError('Wrong command')
+
+        with open(address_book_file, 'w') as writefile:
+            target = address_book[0]['address']
+            new_info = {new_name:new_phone_number}
+            target.append(new_info)
+            json.dump(address_book, writefile, indent=4)     #   Putting into data.json new data
+
+    elif choice_of_adding_name != 'exit':   #   if command == exit: stop executing
+        raise NameError('Wrong command')    #   Raises NameError if command isn't neither 'new' nor 'exit'
