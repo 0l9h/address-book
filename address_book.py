d5a4bce517ec538d16f7d04eb4d3f8f2fd53cbd1
0l9h <olehbobeliak@gmail.com>
2020-04-23 23:22:52 +0300

    Created NameValidation func (len(new_name) should be >= 3)

--- address_book.py
+++ address_book.py
@@ -8,7 +8,7 @@ with open(address_book_file, 'r') as readfile:
 
 status = False
 
-user_key = input('Input name to get phone number:\n')
+user_key = input('Input name to get phone number:\n').title()
 
 
 def Loop():
@@ -27,23 +27,47 @@ if status == False:     #   if phone [user_key] doesn't exists
 If you don\'t want to add this name, print "exit":\n')
     
     if choice_of_adding_name == 'new':
-        new_name = user_key     #   Creating of new address_book member with [user_key] name
+        new_name = user_key    #   Creating of new address_book member with [user_key] name
         new_phone_number = input('Input new number:\n')     #   Creating of new data
 
-        def CheckValidation(new_phone_number):
-            CheckValidationStatus = True
+
+        
+        def CheckPhoneValidation(new_phone_number):
+            CheckPhoneValidationStatus = True
             if len(new_phone_number) == 10:     #   Check if new_phone_number contains 10 chars
                 for char in new_phone_number:   #   Loop through each char of new_phone_number
                     if ord(char) >= 48 and ord(char) <= 57:     #   Check if each char is a digit (0-9)
                         pass
                     else:
-                        CheckValidationStatus = False       #   Returns False if at least one of chars isn't a digit (0-9)
+                        CheckPhoneValidationStatus = False       #   Returns False if at least one of chars isn't a digit (0-9)
+                        print('Phone number should contain only 0-9 chars')
+                        break
             else:
-                CheckValidationStatus = False       #   Returns False if len(new_phone_number) != 10
-            return CheckValidationStatus
+                CheckPhoneValidationStatus = False       #   Returns False if len(new_phone_number) != 10
+                print('Phone number should contain 10 chars')
+            return CheckPhoneValidationStatus
 
 
-        if CheckValidation(new_phone_number):       #   If CheckValidation() returns True:
+        def NameValidation(new_name):
+            NameValidationStatus = True
+            if len(new_name) >= 3:     #   Check if name is longer than 2 chars
+                for char in new_name:   #   Loop through each char of new_name
+                    if (ord(char) >= 65 and ord(char) <= 90) or (ord(char) >= 97 and ord(char) <= 122):     #   Check if each char is a letter a-z/A-Z
+                        pass
+                    else:
+                        NameValidationStatus = False       #   Returns False if at least one of chars isn't a letter a-z/A-Z
+                        print('Name should contain only a-z/A-Z chars')
+                        break
+            else:
+                NameValidationStatus = False       #   Returns False if len(new_name) < 3
+                print('Name is too short')
+            return NameValidationStatus
+
+        #NameValidation(new_name)
+        #CheckPhoneValidation(new_phone_number)
+
+
+        if NameValidation(new_name) and CheckPhoneValidation(new_phone_number):       #   If both CheckPhoneValidation() and NameValidation(new_name) return True:
             with open(address_book_file, 'w') as writefile:
                 target = address_book[0]['address']
                 new_info = {new_name:new_phone_number}
@@ -51,8 +75,8 @@ If you don\'t want to add this name, print "exit":\n')
                 json.dump(address_book, writefile, indent=4)     #   Putting into data.json new data
                 print('New user succesfully added')
 
-        else:                                       #   If CheckValidation() returns False:
-            print('Invalid phone number')           
+        else:                                       #   If CheckPhoneValidation() or NameValidation() returns False:
+            print('Invalid data')           
 
     elif choice_of_adding_name != 'exit':   #   if command == exit: stop executing
         raise NameError('Wrong command')    #   Raises NameError if command isn't neither 'new' nor 'exit'
